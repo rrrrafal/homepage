d17fe0801f074d67f3f06d1cc835e847b31623bc
Rafal Romanczuk <rrrrafal@vp.pl>
2023-01-21 11:38:59 +0100

    Added two funcions

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
-let photo = document.querySelector(".js-photo");
-let button = document.querySelector(".js-button");
-let nameBackground = document.querySelector(".js-nameBackground");
-let nameHideShow = document.querySelector(".js-nameHideShow");
-let body = document.querySelector(".js-body");
+{
+    const button = document.querySelector(".js-button");
 
-button.addEventListener("click", () => {
-    photo.classList.toggle("header__photo--hide");
-    body.classList.toggle("body--brightBackground");
-    button.classList.toggle("body--brightBackground");
+    const togglePhotoBackgroundButton = () => {
+        const photo = document.querySelector(".js-photo");
+        const nameBackground = document.querySelector(".js-nameBackground");
+        const nameHideShow = document.querySelector(".js-button--nameHideShow");
+        const body = document.querySelector(".js-body");
 
-    if (photo.classList.contains("photo--hide")) {
-        nameBackground.innerText = "ciemne";
-        nameHideShow.innerText = "Pokaż";
-    } else {
-        nameBackground.innerText = "jasne";
-        nameHideShow.innerText = "Ukryj";
+        photo.classList.toggle("header__photo--hide");
+        body.classList.toggle("body--brightBackground");
+        button.classList.toggle("body--brightBackground");
+
+        if (photo.classList.contains("header__photo--hide")) {
+            nameBackground.innerText = "ciemne";
+            nameHideShow.innerText = "Pokaż";
+        } else {
+            nameBackground.innerText = "jasne";
+            nameHideShow.innerText = "Ukryj";
+        }
     }
-});
+
+    const init = () => {
+        button.addEventListener("click", togglePhotoBackgroundButton);
+    }
+    init();
+}
